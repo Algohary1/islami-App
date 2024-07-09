dac088e7825d681674d0f3f6fa06b54c905b77f0
moustafa <moustafaalgohary8@gmail.com>
2024-07-10 02:31:32 +0300

    islami home screen

--- lib/home.dart
+++ lib/home.dart
@@ -1,16 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomeScreen extends StatelessWidget {
-  static const String routeName='HomeScreen';
+  static const String routeName = 'HomeScreen';
+
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Islami'),
+    return Stack(children: [
+      Image.asset('assets/images/main_bg.png'),
+      Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          centerTitle: true,
+          title: Text('إسلامي',
+              style: GoogleFonts.elMessiri(
+                  fontSize: 30, fontWeight: FontWeight.w700)),
+        ),
       ),
-    );
-
+    ]);
   }
 }
