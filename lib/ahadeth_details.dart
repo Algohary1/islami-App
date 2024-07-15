91668e848e5fb433d08dbb90597f0b5dafef263c
moustafa <moustafaalgohary8@gmail.com>
2024-07-15 21:05:56 +0300

    hadeth edit

--- lib/ahadeth_details.dart
+++ lib/ahadeth_details.dart
@@ -4,7 +4,7 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:sebha/ahadeth_model.dart';
 
 class ahadethDetails extends StatefulWidget {
-  static const String routeName='ahadethDetails';
+  static const String routeName = 'ahadethDetails';
   const ahadethDetails({super.key});
 
   @override
@@ -12,27 +12,28 @@ class ahadethDetails extends StatefulWidget {
 }
 
 class _ahadethDetailsState extends State<ahadethDetails> {
-  List<String>verses=[];
+  List<String> verses = [];
   @override
   Widget build(BuildContext context) {
-    var model=ModalRoute.of(context)?.settings.arguments as ahadethModel;
-    if(verses.isEmpty){
+    var model = ModalRoute.of(context)?.settings.arguments as ahadethModel;
+    if (verses.isEmpty) {
       loadahadethFile(model.index);
-
     }
     return Container(
       decoration: BoxDecoration(
-          image: DecorationImage(image: AssetImage('assets/images/main_bg.png'), fit: BoxFit.fill)
-      ),
+          image: DecorationImage(
+              image: AssetImage('assets/images/main_bg.png'),
+              fit: BoxFit.fill)),
       child: Scaffold(
         backgroundColor: Colors.transparent,
         appBar: AppBar(
           centerTitle: true,
           backgroundColor: Colors.transparent,
-          title: Text(model.name, style: GoogleFonts.elMessiri(
-              fontWeight: FontWeight.w700,
-              fontSize: 30
-          ),),
+          title: Text(
+            model.name,
+            style: GoogleFonts.elMessiri(
+                fontWeight: FontWeight.w700, fontSize: 30),
+          ),
         ),
         body: Card(
           color: Color(0xCCF8F8F8),
@@ -41,21 +42,21 @@ class _ahadethDetailsState extends State<ahadethDetails> {
               borderRadius: BorderRadius.circular(25),
               borderSide: BorderSide(
                 color: Colors.transparent,
-              )
-          ),
-
-          child: ListView.builder(itemBuilder: (context, index) {
-            return Padding(
-              padding: const EdgeInsets.all(10),
-              child: Text(
-                '${verses[index]}(${index+1})',
-                textAlign: TextAlign.right,style: GoogleFonts.inter(
-                fontSize: 20,
-                fontWeight: FontWeight.w400,
-
-              ),),
-            );
-          },
+              )),
+          child: ListView.builder(
+            itemBuilder: (context, index) {
+              return Padding(
+                padding: const EdgeInsets.all(10),
+                child: Text(
+                  '${verses[index]}(${index + 1})',
+                  textAlign: TextAlign.right,
+                  style: GoogleFonts.inter(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+              );
+            },
             itemCount: verses.length,
           ),
         ),
@@ -63,14 +64,13 @@ class _ahadethDetailsState extends State<ahadethDetails> {
     );
   }
 
-  loadahadethFile(int index)async{
-    String ahadeth= await rootBundle.loadString('assets/files/ahadeth/${index+1}.txt');
+  loadahadethFile(int index) async {
+    String ahadeth =
+        await rootBundle.loadString('assets/files/ahadeth/${index + 1}.txt');
 
-    List<String>ahadethLines=ahadeth.split('\n');
+    List<String> ahadethLines = ahadeth.split('\n');
     print(ahadethLines);
-    verses=ahadethLines;
-    setState(() {
-
-    });
+    verses = ahadethLines;
+    setState(() {});
   }
 }
