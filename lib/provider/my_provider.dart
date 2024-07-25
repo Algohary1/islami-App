54698ddb9c22e6cf1a0d30e6ca312940ee678bf2
Moustafa Algohary <moustafaalgohary8@gmail.com>
2024-07-25 23:47:04 +0300

    islami Shared preference added

--- lib/provider/my_provider.dart
+++ lib/provider/my_provider.dart
@@ -1,10 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class MyProvider extends ChangeNotifier{
   ThemeMode mode=ThemeMode.light;
 
-  changeTheme(ThemeMode themeMode){
+  Future<void> getTheme()async{
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+bool? isdark=prefs.getBool('isdark');
+
+if(isdark!=null){
+  if(isdark=true){
+    mode=ThemeMode.dark;
+  }else{
+    mode=ThemeMode.light;
+  }
+notifyListeners();
+}
+
+
+
+  }
+  Future<void>changeTheme(ThemeMode themeMode)async{
     mode=themeMode;
+
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setBool('isdark', mode==ThemeMode.dark);
+
+
+
+
     notifyListeners();
 
   }
